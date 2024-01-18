c27d161c8a957a636502aa1f8536c6cb181e4194
Sam <138595608+AIWizard47@users.noreply.github.com>
2024-01-18 17:13:06 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Details(models.Model):
+    name = models.CharField(max_length =100)
+    age = models.IntegerField(2)
+    height = models.IntegerField(3)
+    weight = models.IntegerField(3)
--- views.py
+++ views.py
@@ -1,24 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 import json
+from .models import Details
 
 # Create your views here.
 def index(request):
-
+    det = Details.objects.all()
     sam = {
-        'name' : 'Sam',
-        'age' : 20,
+        'name' : det[0].name,
+        'age' : det[0].age,
         'college' :'IES University',
         'course' : 'B-Tech'
     }
     return render(request,'index.html',sam)
 
 def API(request):
-    name = str(request.GET['name'])
-    age = str(request.GET['age'])
-    height = str(request.GET['height'])
-    weight = str(request.GET['weight'])
-    amount_of_word = len(name.split())
+    # name = str(request.POST['name'])
+    # age = str(request.POST['age'])
+    # height = str(request.POST['height'])
+    # weight = str(request.POST['weight'])
+    # amount_of_word = len(name.split())
+    
     # return render(request,'download.html',{'amount':word})
-    data_set = {'words': name, 'amount': str(amount_of_word),'age': age ,'height': height, 'weight': weight}
-    return JsonResponse(data_set)
+    
+    data_sets = []
+
+    # Retrieve details from the Details model
+    details_objects = Details.objects.all()
+
+    # Iterate through each detail object and add to the data_sets list
+    for detail_object in details_objects:
+        data_set = {
+            'details':{
+            'id':detail_object.id,
+            'name': detail_object.name,
+            'age': detail_object.age,
+            'height': detail_object.height,
+            'weight': detail_object.weight}
+        }
+        data_sets.append(data_set)
+
+    # Return the data_sets list as a JSON response
+    return JsonResponse(data_sets, safe=False)
